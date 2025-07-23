c9a65769723c22f792f6bdd99e1bbd780dbb7518
swethhha <swetha06112004@gmail.com>
2025-07-23 11:16:49 +0530

    x

---
+++ Day1/Day1proj2/Models/SupportAgent.cs
@@ -0,0 +1,21 @@
+﻿namespace Day1proj2.Models
+{
+    public class SupportAgent
+    {
+        public int AgentId { get; }
+        public string Name { get; }
+        public string Department { get; }
+
+        public SupportAgent(int agentId, string name, string department)
+        {
+            AgentId = agentId;
+            Name = name;
+            Department = department;
+        }
+
+        public void DisplayAgent()
+        {
+            Console.WriteLine($"Agent ID: {AgentId}, Name: {Name}, Department: {Department}");
+        }
+    }
+}
